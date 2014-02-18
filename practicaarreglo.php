8c156dae835cf456f0061fb2a3586bf1575d1954
zulma <zulmatec@hotmail.com>
2014-02-18 10:05:27 -0600

    ejemplo

---
+++ practicaarreglo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<?php
+$valor_vector = array('nombre'=>'david','edad'=>'89','sexo'=>'masculino','depto'=>'ventas','sueldo'=>
+
+?>
+</body>
+</html>
